460dc46dccd63df61e5d2e282812d6864fe7b6a3
godenji <admin@ushr.com>
2020-08-26 17:21:54 +0700

    formatting

--- bindings/app/godenji/iso/bindable/Form.scala
+++ bindings/app/godenji/iso/bindable/Form.scala
@@ -8,13 +8,15 @@ object Form {
   /**
    * materialize a value class form bindable
    */
-  implicit final def formBindValueClass[T <: MappedToBase](implicit formBind: Formatter[T#Underlying], iso: Isomorphism[T]): Formatter[T] = new Formatter[T] {
+  implicit final def formBindValueClass[T <: MappedToBase]
+    (implicit formBind: Formatter[T#Underlying], iso: Isomorphism[T]): Formatter[T] = new Formatter[T] {
+
     override def bind(key: String, data: Map[String, String]): Seq[FormError] Either T =
       formBind.bind(key, data).fold(
         Left(_), s => Right(iso.comap(s))
       )
 
-    def unbind(key: String, id: T): Map[String, String] =
+    override def unbind(key: String, id: T): Map[String, String] =
       formBind.unbind(key, iso.map(id))
   }
 
